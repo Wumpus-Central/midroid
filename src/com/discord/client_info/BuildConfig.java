31c4885629989b714f2a99efa09ee1ebf67b4647
DiscrapperManager <admin@minerpl.xyz>
2025-05-07 08:54:05 +0000

    🚀 beta release 279.9 (279109)

--- src/com/discord/client_info/BuildConfig.java
+++ src/com/discord/client_info/BuildConfig.java
@@ -4,9 +4,9 @@ public final class BuildConfig {
    public static final String BUILD_TYPE = "release";
    public static final boolean DEBUG = false;
    public static final String LIBRARY_PACKAGE_NAME = "com.discord.client_info";
-   public static final String PROGUARD_UUID = "b8cde2ec-193e-473a-ad65-5523986e8d62";
+   public static final String PROGUARD_UUID = "ed527a15-a921-4644-a4ba-419f1a98d634";
    public static final String SENTRY_ALPHA_BETA_DSN = "https://9a42ef460144a03b30c8b2d5321cfe11@o64374.ingest.sentry.io/5992375";
    public static final String SENTRY_DSN = "https://70545531dfe34835bf4dd0996821e8b6@o64374.ingest.sentry.io/5992375";
-   public static final String SENTRY_RELEASE = "discord_android@279.8.0-1+279108";
+   public static final String SENTRY_RELEASE = "discord_android@279.9.0-1+279109";
    public static final String SENTRY_STAFF_DSN = "https://90509cba01573ee4e14a2f5e15aee5ca@o64374.ingest.sentry.io/5992375";
 }
