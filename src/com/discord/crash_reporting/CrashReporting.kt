e29b373fd7cd4b43ab6111a2179db08cf00569eb
DiscrapperManager <admin@minerpl.xyz>
2025-05-16 08:53:12 +0000

    🚀 beta release 280.10 (280110)

--- src/com/discord/crash_reporting/CrashReporting.kt
+++ src/com/discord/crash_reporting/CrashReporting.kt
@@ -97,7 +97,7 @@ public object CrashReporting {
       var5.setEnableAutoActivityLifecycleTracing(false);
       var5.setTracesSampleRate(0.0);
       var5.setSampleRate(var3);
-      var5.setProguardUuid("0f454f1d-3a46-4386-8944-3cac381c7498");
+      var5.setProguardUuid("45134a3f-af51-4164-b54e-12322c5144f0");
       var5.setTag("buildNumber", var7.getVersionCode());
       var5.setTag("appVersion", var7.getVersionName());
       var5.setBeforeSend(new a(var2));
