427b9f7aa35c4471e0e86a9f8d16d2d0e2f8cb6c
DiscrapperManager <admin@minerpl.xyz>
2025-05-16 20:46:40 +0000

    🚀 beta release 280.11 (280111)

--- src/com/discord/BuildConfig.java
+++ src/com/discord/BuildConfig.java
@@ -10,9 +10,9 @@ public final class BuildConfig {
    public static final boolean IS_HERMES_ENABLED = true;
    public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
    public static final String MAIN_MODULE = "index.native";
-   public static final String USER_AGENT = "Discord-Android/280110";
-   public static final int VERSION_CODE = 280110;
-   public static final String VERSION_NAME = "280.10 - Beta";
-   public static final String VERSION_NAME_RNA = "280.10";
+   public static final String USER_AGENT = "Discord-Android/280111";
+   public static final int VERSION_CODE = 280111;
+   public static final String VERSION_NAME = "280.11 - Beta";
+   public static final String VERSION_NAME_RNA = "280.11";
    public static final Boolean logTTIMetrics = Boolean.FALSE;
 }
