f397d78615865a6f9e9fc6af2a288898405b2792
DiscrapperManager <admin@minerpl.xyz>
2025-05-17 07:41:55 +0000

    🚀 alpha release 281.5 (281205)

--- src/com/discord/client_info/BuildConfig.java
+++ src/com/discord/client_info/BuildConfig.java
@@ -4,9 +4,9 @@ public final class BuildConfig {
    public static final String BUILD_TYPE = "release";
    public static final boolean DEBUG = false;
    public static final String LIBRARY_PACKAGE_NAME = "com.discord.client_info";
-   public static final String PROGUARD_UUID = "e86b1f32-effe-47ed-974d-02291ccf3037";
+   public static final String PROGUARD_UUID = "3a2c9835-2e33-4441-b792-106146f1fd42";
    public static final String SENTRY_ALPHA_BETA_DSN = "https://9a42ef460144a03b30c8b2d5321cfe11@o64374.ingest.sentry.io/5992375";
    public static final String SENTRY_DSN = "https://70545531dfe34835bf4dd0996821e8b6@o64374.ingest.sentry.io/5992375";
-   public static final String SENTRY_RELEASE = "discord_android@281.4.0-2+281204";
+   public static final String SENTRY_RELEASE = "discord_android@281.5.0-2+281205";
    public static final String SENTRY_STAFF_DSN = "https://90509cba01573ee4e14a2f5e15aee5ca@o64374.ingest.sentry.io/5992375";
 }
--- src/com/discord/client_info/ClientInfo.kt
+++ src/com/discord/client_info/ClientInfo.kt
@@ -6,10 +6,10 @@ import kotlin.jvm.internal.q
 import o8.l
 
 public object ClientInfo {
-   public const val PROGUARD_UUID: String = "e86b1f32-effe-47ed-974d-02291ccf3037"
+   public const val PROGUARD_UUID: String = "3a2c9835-2e33-4441-b792-106146f1fd42"
    public const val SENTRY_ALPHA_BETA_DSN: String = "https://9a42ef460144a03b30c8b2d5321cfe11@o64374.ingest.sentry.io/5992375"
    public const val SENTRY_DSN: String = "https://70545531dfe34835bf4dd0996821e8b6@o64374.ingest.sentry.io/5992375"
-   public const val SENTRY_RELEASE: String = "discord_android@281.4.0-2+281204"
+   public const val SENTRY_RELEASE: String = "discord_android@281.5.0-2+281205"
    public const val SENTRY_STAFF_DSN: String = "https://90509cba01573ee4e14a2f5e15aee5ca@o64374.ingest.sentry.io/5992375"
    private final lateinit var buildType: String
    private final lateinit var flavor: String
