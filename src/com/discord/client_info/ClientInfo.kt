931e87f2829e17a41f3667500721ca93704df100
DiscrapperManager <admin@minerpl.xyz>
2025-05-23 22:47:02 +0000

    🚀 beta release 281.13 (281113)

--- src/com/discord/client_info/ClientInfo.kt
+++ src/com/discord/client_info/ClientInfo.kt
@@ -63,7 +63,7 @@ public object ClientInfo {
    public const val SENTRY_DSN: String = "https://70545531dfe34835bf4dd0996821e8b6@o64374.ingest.sentry.io/5992375"
    public const val SENTRY_STAFF_DSN: String = "https://90509cba01573ee4e14a2f5e15aee5ca@o64374.ingest.sentry.io/5992375"
    public const val SENTRY_ALPHA_BETA_DSN: String = "https://9a42ef460144a03b30c8b2d5321cfe11@o64374.ingest.sentry.io/5992375"
-   public const val SENTRY_RELEASE: String = "discord_android@281.11.0-1+281111"
+   public const val SENTRY_RELEASE: String = "discord_android@281.13.0-1+281113"
 
    public final val isDebugBuild: Boolean
       public final get() {
@@ -108,7 +108,7 @@ public object ClientInfo {
       }
 
 
-   public const val PROGUARD_UUID: String = "47dd8359-bbe0-4807-99d2-0878949ec4a5"
+   public const val PROGUARD_UUID: String = "fd0c2413-53c1-4f9a-9e08-fe8819c27a77"
 
    @JvmStatic
    fun `isProdBuild_delegate$lambda$1`(): Boolean {
--- src/com/discord/crash_reporting/CrashReporting.kt
+++ src/com/discord/crash_reporting/CrashReporting.kt
@@ -91,7 +91,7 @@ public object CrashReporting {
       var5.setEnableAutoActivityLifecycleTracing(false);
       var5.setTracesSampleRate(0.0);
       var5.setSampleRate(var3);
-      var5.setProguardUuid("47dd8359-bbe0-4807-99d2-0878949ec4a5");
+      var5.setProguardUuid("fd0c2413-53c1-4f9a-9e08-fe8819c27a77");
       var5.setTag("buildNumber", var7.getVersionCode());
       var5.setTag("appVersion", var7.getVersionName());
       var5.setBeforeSend(new a(var2));
