50491839648620082431ea29ce6a0cc9511cab3f
DiscrapperManager <admin@minerpl.xyz>
2025-07-19 08:53:52 +0000

    🚀 beta release 289.19 (289119)

--- src/com/discord/BuildConfig.java
+++ src/com/discord/BuildConfig.java
@@ -10,9 +10,9 @@ public final class BuildConfig {
    public static final boolean IS_HERMES_ENABLED = true;
    public static final boolean IS_NEW_ARCHITECTURE_ENABLED = false;
    public static final String MAIN_MODULE = "index.native";
-   public static final String USER_AGENT = "Discord-Android/289118";
-   public static final int VERSION_CODE = 289118;
-   public static final String VERSION_NAME = "289.18 - Beta";
-   public static final String VERSION_NAME_RNA = "289.18";
+   public static final String USER_AGENT = "Discord-Android/289119";
+   public static final int VERSION_CODE = 289119;
+   public static final String VERSION_NAME = "289.19 - Beta";
+   public static final String VERSION_NAME_RNA = "289.19";
    public static final Boolean logTTIMetrics = Boolean.FALSE;
 }
--- src/com/discord/crash_reporting/CrashReporting.kt
+++ src/com/discord/crash_reporting/CrashReporting.kt
@@ -92,7 +92,7 @@ public object CrashReporting {
       var5.setEnableAutoActivityLifecycleTracing(false);
       var5.setTracesSampleRate(0.0);
       var5.setSampleRate(var3);
-      var5.setProguardUuid("dd9b647d-7a55-4fda-a363-77574af79bcc");
+      var5.setProguardUuid("8a324a99-e6cb-4a26-b70b-5b80b8b5fea1");
       var5.setTag("buildNumber", var7.getVersionCode());
       var5.setTag("appVersion", var7.getVersionName());
       var5.setBeforeSend(new a(var2));
